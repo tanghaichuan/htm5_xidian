514147e026ee7a523440c96e685c7a44b7ba0ef4
tanghaichuan <764505165@qq.com>
2016-11-28 14:50:38 +0800

    爱尚西点网站v1.0静态页

--- 爱尚西点网站v1.0静态页/前台静态页/js/homeEdit.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(".dropdown").mouseover(function(){
-		$("ul",this).css("display","block");
-	});
-	$(".dropdown").mouseleave(function(){
-		$("ul",this).css("display","none");
-	});
-});

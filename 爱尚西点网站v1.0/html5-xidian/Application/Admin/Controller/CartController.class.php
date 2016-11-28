b4b689d2dc164fa325398dca10a74049ee70b04a
tanghaichuan <764505165@qq.com>
2016-11-28 14:27:16 +0800

    爱尚西点网站v1.0

--- 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/CartController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/CartController.class.php
@@ -2,6 +2,17 @@
 namespace Admin\Controller;
 use Think\Controller;
 class CartController extends Controller {
+	public function __construct(){
+		parent::__construct();
+		if(!isLogin()){
+			$this->error("请先登录",U("Admin/login/index"));
+		}
+		else{ 
+			$userModel=M('admin_users');
+			$this->assign('username',$_SESSION['username']);
+		}
+	}
+	
     public function index(){
        $this->display("list");
     }
--- 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/FoodController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/FoodController.class.php
@@ -2,6 +2,16 @@
 namespace Admin\Controller;
 use Think\Controller;
 class FoodController extends Controller {
+    public function __construct(){
+        parent::__construct();
+        if(!isLogin()){
+            $this->error("请先登录",U("Admin/login/index"));
+        }
+        else{ 
+            $userModel=M('admin_users');
+            $this->assign('username',$_SESSION['username']);
+        }
+    }
     public function index(){
        $this->display("list");
     }

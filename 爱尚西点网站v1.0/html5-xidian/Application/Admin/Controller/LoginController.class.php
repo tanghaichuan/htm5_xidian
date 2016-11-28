7078d02211f5d64edbb28295a915684dc028cc8d
tanghaichuan <764505165@qq.com>
2016-11-28 13:51:37 +0800

    爱尚西点网站v1.0

--- 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/LoginController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/LoginController.class.php
@@ -3,9 +3,27 @@ namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
     public function index(){
-
        $this->display("login");
     }
+
+    public function login(){
+        $Model=M("admin_users");
+        $user=$Model->where(array('username'=>I('username')))->find(); 
+        if(!$user || $user['password']!= I('password','','md5')){ 
+                $this->error("账号或者密码错误！");
+        }
+        else{
+            $data=array(
+                'last_login_time'=>date('Y-m-d H-i-s')
+                );
+            $Model->where(array('username'=>I('username')))->save($data);
+            session('uid',$user['id']);
+            session('password',$user['password']);
+            session('username',$user['username']);
+            $this->success("登录中...",U("index/index"));
+        }
+    }
+
 
     public function register(){
     	$this->display();
@@ -14,7 +32,7 @@ class LoginController extends Controller {
     public function register_add(){
     	$adminModel=D("AdminUsers");
     	if ($adminModel->create() && $adminModel->add()) {
-    			$this->success("插入成功！", U('Admin/login/index'));
+    			$this->success("注册成功！", U('Admin/login/index'));
     		}
     		else {
     			$this->error($adminModel->getError());

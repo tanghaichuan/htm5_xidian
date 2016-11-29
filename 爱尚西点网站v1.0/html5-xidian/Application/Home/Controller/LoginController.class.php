6057ae8a1a4fc641b921e33a78b5f01440e1588b
qianyuming <410865666@qq.com>
2016-11-29 11:01:27 +0800

    爱尚西点v1.0

--- 爱尚西点网站v1.0/html5-xidian/Application/Home/Controller/LoginController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Home/Controller/LoginController.class.php
@@ -3,10 +3,24 @@ namespace Home\Controller;
 use Think\Controller;
 class LoginController extends Controller {
     public function index(){
-       $this->display();
+       $this->display("login");
     }
     public function login(){
-    	$this->display();
+    	$Model=M("admin_users");
+        $user=$Model->where(array('username'=>I('username')))->find(); 
+        if(!$user || $user['password']!= I('password','','md5')){ 
+                $this->error("账号或者密码错误！");
+        }
+        else{
+            $data=array(
+                'last_login_time'=>date('Y-m-d H-i-s')
+                );
+            $Model->where(array('username'=>I('username')))->save($data);
+            session('uid',$user['id']);
+            session('password',$user['password']);
+            session('username',$user['username']);
+            $this->success("登录中...",U("index/index"));
+        }
     }
     public function register(){
     	$this->display();

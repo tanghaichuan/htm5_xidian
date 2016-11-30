0d2921b1aa3f6caa370017d478feb809da802aa0
tanghaichuan <764505165@qq.com>
2016-11-30 09:15:22 +0800

    爱尚西点网站v1.0

--- 爱尚西点网站v1.0/html5-xidian/Application/Home/Controller/IndexController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Home/Controller/IndexController.class.php
@@ -2,6 +2,14 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+	public function __construct(){
+        parent::__construct();
+        if(isLogin()){
+            $userModel=M('public_users');
+            $this->assign('username',$_COOKIE['username']);
+        }
+    }
+
     public function index(){
     	$this->display();
     }
--- 爱尚西点网站v1.0/html5-xidian/Public/home/js/homeEdit.js
+++ 爱尚西点网站v1.0/html5-xidian/Public/home/js/homeEdit.js
@@ -5,4 +5,28 @@ $(function(){
 	$(".dropdown").mouseleave(function(){
 		$("ul",this).css("display","none");
 	});
+
+
+	function getCookie(cookie_name){  
+      var allcookies = document.cookie;  
+      var cookie_pos = allcookies.indexOf(cookie_name);   
+      //索引的长度  
+      // 如果找到了索引，就代表cookie存在，  
+      // 反之，就说明不存在。  
+      if (cookie_pos != -1){  
+      // 把cookie_pos放在值的开始，只要给值加1即可。  
+        cookie_pos += cookie_name.length + 1;        
+        var cookie_end = allcookies.indexOf(";", cookie_pos);    
+        if (cookie_end == -1){  
+          cookie_end = allcookies.length;  
+        }  
+        var value = unescape(allcookies.substring(cookie_pos, cookie_end));  
+      }
+      return value;  
+    }
+    var cookie=getCookie("username");
+    if(cookie){
+    	$("#isLogin").html("欢迎："+cookie);
+    }
+    
 });

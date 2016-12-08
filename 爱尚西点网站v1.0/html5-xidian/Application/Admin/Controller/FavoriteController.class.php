493014daf9e391483be0706da4de79edf1b44def
tanghaichuan <764505165@qq.com>
2016-12-08 09:12:07 +0800

    爱尚西点网站v1.0

--- 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/FavoriteController.class.php
+++ 爱尚西点网站v1.0/html5-xidian/Application/Admin/Controller/FavoriteController.class.php
@@ -20,10 +20,29 @@ class FavoriteController extends Controller {
     	$choose=$model->find($id);
     	$this->assign("choose",$choose);
     	$this->assign("userList",$userList);
+        $favorModel=M("favorite");
+        $foodList = $favorModel->where(array("user_name"=>$choose['username']))->join('foods on favorite.food_id=foods.id')->select();
+        import('Org.Util.Page');
+        $count=count($foodList);
+        $page = new \Think\Page($count,5);
+        $nowPage = isset($_GET['p'])?intval($_GET['p']):1;
+        $page -> setConfig('first','第一页');
+        $page -> setConfig('prev','<<');
+        $page -> setConfig('next','>>');
+        $foodList = $favorModel->where(array("user_name"=>$choose['username']))->join('foods on favorite.food_id=foods.id') -> order('id desc') -> page($nowPage.',5') -> select();
+        $show = $page -> show();
+        $this -> assign('page',$show);
+        $this -> assign('foodList',$foodList); 
         $this->display();
     }
 
-    public function manageFav(){
-    	
+    public function del(){
+    	$id = isset($_GET['id']) ? intval($_GET['id']) : '';
+        if(M("favorite")->where(array('food_id'=>$id))->delete()){
+            $this->success("删除成功!",U("favorite/index"));
+        }
+        else{
+            $this->error("删除失败!");
+        }
     }
 }
